e437a8602e49fafaa61fcfb2540059c40ededcff
Sumitkumar1304 <badsjsumitkumar@gmail.com>
2026-01-08 01:43:59 +0530

    userinput

---
+++ takeuserinput.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# In this section we will learn about taking input form user
+
+
+echo "Writ your name: "
+
+read name
+
+echo "Hello Mr. $name , Welcome to Learning Devops series of Shellscripting"
